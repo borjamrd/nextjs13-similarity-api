be091bf5969f0e4718450689b25f27d74dc08ba6
Borja Muñoz <90717945+borjamrd@users.noreply.github.com>
2023-04-01 10:41:06 +0200

    similarity API succesfully created

--- src/pages/api/v1/similarity.ts
+++ src/pages/api/v1/similarity.ts
@@ -1,14 +1,14 @@
-import { cosineSimilarity } from "@/helpers/cosine-similarity"
-import { withMethods } from "@/lib/api-middlewares/with-methods"
-import { db } from "@/lib/db"
-import { openai } from "@/lib/openai"
-import { NextApiRequest, NextApiResponse } from "next"
+import { cosineSimilarity } from '@/helpers/cosine-similarity'
+import { withMethods } from '@/lib/api-middlewares/with-methods'
+import { db } from '@/lib/db'
+import { openai } from '@/lib/openai'
+import { NextApiRequest, NextApiResponse } from 'next'
 import { z } from 'zod'
+
 const reqSchema = z.object({
     text1: z.string().max(1000),
-    text2: z.string().max(1000)
+    text2: z.string().max(1000),
 })
-
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     const body = req.body as unknown
@@ -18,46 +18,47 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
         return res.status(401).json({ error: 'Unauthorized' })
     }
 
-
     try {
         const { text1, text2 } = reqSchema.parse(body)
-        const validApiKey = await db.apiKey.findFirs({
+
+        const validApiKey = await db.apiKey.findFirst({
             where: {
                 key: apiKey,
                 enabled: true,
-            }
+            },
         })
+
+
+
         if (!validApiKey) {
             return res.status(401).json({ error: 'Unauthorized' })
         }
 
         const start = new Date()
-
         const embeddings = await Promise.all(
             [text1, text2].map(async (text) => {
                 const res = await openai.createEmbedding({
                     model: 'text-embedding-ada-002',
                     input: text,
                 })
+
                 return res.data.data[0].embedding
             })
         )
-
         const similarity = cosineSimilarity(embeddings[0], embeddings[1])
 
         const duration = new Date().getTime() - start.getTime()
 
-        //persist request
-
+        // Persist request
         await db.apiRequest.create({
             data: {
                 duration,
-                method: req.method,
+                method: req.method as string,
                 path: req.url as string,
                 status: 200,
                 apiKeyId: validApiKey.id,
-                usedApiKey: validApiKey.key
-            }
+                usedApiKey: validApiKey.key,
+            },
         })
 
         return res.status(200).json({ success: true, text1, text2, similarity })
@@ -65,9 +66,9 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
         if (error instanceof z.ZodError) {
             return res.status(400).json({ error: error.issues })
         }
-        return res.status(500).json({ error: 'Internal server error' })
+
+        return res.status(500).json({ error: 'Internal server error', description: error })
     }
 }
-
 
 export default withMethods(['POST'], handler)
